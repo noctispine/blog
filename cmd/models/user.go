4ce296486d0c0671859cdc343e96cf3db4690d32
Eren Çam <eren@Erens-MacBook-Air.local>
2022-10-14 02:04:33 +0300

    chore: seperate user model

--- cmd/models/user.go
+++ cmd/models/user.go
@@ -4,20 +4,6 @@ import (
 	"time"
 )
 
-// type User123 struct {
-// 	ID     uint   `json:"id" pg:"id"`
-// 	FirstName     string `json:"firstName" pg:"first_name" `
-// 	LastName     string `json:"lastName" pg:"last_name"`
-// 	Email    string `json:"email" pg:"email"`
-// 	PasswordHash string `json:"passwordHash,omitempty" pg:"password_hash"`
-// 	RegisteredAt time.Time `json:"-" gorm:"column:registered_at"`
-// 	LastLogin time.Time `json:"-" gorm:"column:last_login_at"`
-// 	IntroDesc string `json:"introDesc" pg:"intro_desc"`
-// 	Role     int    `json:"role" pg:"role"`
-// 	ProfileDesc string `json:"profileDesc" pg:"profile_desc"`
-// 	Token string `json:"token" pg:"token"`
-// }
-
 type UserAccount struct {
 	ID int64 `json:"id"`
 	FirstName string
